da8a8e7b6a971bb43be6c13b1fca0f2ee3f32aa2
Flavya04 <flavya2310@gmail.com>
2025-02-08 11:05:52 -0300

    media_srqt

--- funcoes/otimizador.py
+++ funcoes/otimizador.py
@@ -132,7 +132,7 @@ def calc_aptidao(seq_coef, lista_lista_pos, list_tranfor, fixar_coef,
                     lista_coef_Nash_Sutcliffe = [9999]
 
             # lista_aptidoes.append(round((np.mean(lista_coef_Nash_Sutcliffe)), precisao))
-            lista_aptidoes.append(round(hmean(lista_coef_Nash_Sutcliffe), precisao))
+            lista_aptidoes.append(round(np.sqrt(np.mean(lista_coef_Nash_Sutcliffe**2)), precisao))
 
 
     return lista_aptidoes
